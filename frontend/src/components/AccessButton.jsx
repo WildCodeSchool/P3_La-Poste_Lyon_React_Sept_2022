d8a2cc3157f769e9ca50fb7f7c96fc7ab789c83e
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-12-13 16:03:55 +0100

    AccessButton OP

--- frontend/src/components/AccessButton.jsx
+++ frontend/src/components/AccessButton.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function AccessButton() {
-  return <button type="button">Accéder</button>;
+  return (
+    <button
+      type="button"
+      className="bg-[#003DA5] text-white m-3 py-1 px-4 rounded-lg shadow-lg"
+    >
+      Accéder
+    </button>
+  );
 }
 
 export default AccessButton;
