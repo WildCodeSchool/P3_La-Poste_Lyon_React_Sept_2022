1e7488f07d24f043fbec34509776142e8fd13dcc
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2022-12-14 20:04:56 +0100

    remove components from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,8 @@
 import "./App.css";
 import "./index.css";
-import RegisterFirstStep from "./components/RegisterFirstStep";
-import RegisterSecondStep from "./components/RegisterSecondStep";
-import RegisterThirdStep from "./components/RegisterThirdStep";
-import RegisterFourthStep from "./components/RegisterFourthStep";
 
 function App() {
-  return (
-    <>
-      <RegisterFirstStep />
-      <RegisterSecondStep />
-      <RegisterThirdStep />
-      <RegisterFourthStep />
-    </>
-  );
+  return <>test</>;
 }
 
 export default App;
