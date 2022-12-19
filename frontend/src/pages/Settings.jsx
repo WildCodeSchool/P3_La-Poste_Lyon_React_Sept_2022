6a380edf45b7b60b1fe5f213bbb5998b5b3c616f
arnaudchampetier <arnaud.champetier9@gmail.com>
2022-12-19 10:15:59 +0100

    settings2

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -22,12 +22,12 @@ function Settings() {
       <NavigationBar />
 
       <div className="mt-4 flex justify-center">
-        <h1 className="flex justify-center items-center text-bold text-xl text-white rounded-3xl shadow-lg bg-[#003DA5] w-2/3 h-10 md:text-2xl text-center md:w-1/4 md:h-14 md:text-center">
+        <PreviousButton />
+
+        <h1 className="flex justify-center items-center text-bold text-xl text-black mt-8 w-2/3 h-10 md:text-2xl text-center md:w-1/4 md:h-14 md:text-center">
           Modifier mes informations
         </h1>
-        <div className="mt-12 items-center">
-          <PreviousButton />
-        </div>
+
         {image && (
           <div className="w-64 h-64 rounded-full overflow-hidden mx-auto mt-6">
             <img src={URL.createObjectURL(image)} alt="Uploaded" />
