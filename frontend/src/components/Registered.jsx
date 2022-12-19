4b24433a55fd834a0e89fe177475296c6110e1ed
quentinferraripro <ferrari.quentin@gmail.com>
2022-12-19 10:25:02 +0100

    merge with userCourse

--- frontend/src/components/Registered.jsx
+++ frontend/src/components/Registered.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 
 function Registered() {
-  return (
-    <div className="relative">
-      <h1>Hello world</h1>
-      <div className="absolute bg-opacity-50 rounded-lg shadow-xl">
-        <p>je suis une explication</p>
-      </div>
-      <div className="px-4 py-2 bg-white rounded-lg shadow-xl absolute top-120px">
-        <p>Cliquez sur ce bouton pour enregistrer vos modifications</p>
-      </div>
-    </div>
-  );
+  return <div>Registered</div>;
 }
 
 export default Registered;
