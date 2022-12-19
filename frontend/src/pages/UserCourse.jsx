3ed9ebc82cf8f3da497b88f41fdb7029fd4eb17f
Chloé Bidau <chloebidau@hotmail.fr>
2022-12-19 16:43:17 +0100

    changes on userCourse

--- frontend/src/pages/UserCourse.jsx
+++ frontend/src/pages/UserCourse.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
-import badge1 from "../assets/badge1.png";
+import step1 from "../assets/userCourse/Etape.svg";
+import step2 from "../assets/userCourse/Chemin.svg";
+import wellDone from "../assets/userCourse/WellDone.svg";
 
 function UserCourse() {
   return (
@@ -10,27 +12,107 @@ function UserCourse() {
       <div>
         <div>
           <span>
-            <img src={badge1} alt="badge1" />
+            <img src={step1} alt="Step1" />
           </span>
         </div>
         <div>
           <span>
-            <img src={badge1} alt="badge1" />
+            <img className="h-16" src={wellDone} alt="welldone" />
           </span>
         </div>
         <div>
           <span>
-            <img src={badge1} alt="badge1" />
+            <img src={step2} alt="Way1" />
           </span>
         </div>
         <div>
           <span>
-            <img src={badge1} alt="badge1" />
+            <img src={step1} alt="Step2" />
           </span>
         </div>
         <div>
           <span>
-            <img src={badge1} alt="badge1" />
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step3" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step4" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step5" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step6" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step7" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step8" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step9" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step1} alt="Step10" />
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src={step2} alt="badge1" />
           </span>
         </div>
       </div>
