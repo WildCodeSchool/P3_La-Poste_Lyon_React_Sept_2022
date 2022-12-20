769b0ac1c03c663e61c9f6f93d801758fe50477b
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-12-20 09:13:58 +0100

    Adding minus button

--- frontend/src/pages/SearchUsers.jsx
+++ frontend/src/pages/SearchUsers.jsx
@@ -104,20 +104,37 @@ function SearchUsers() {
                     className=""
                     onClick={() => handleSelectedUser(index)}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 19 19"
-                      strokeWidth="1.5"
-                      stroke="#003DA5"
-                      className="w-4 h-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 6v12m6-6H6"
-                      />
-                    </svg>
+                    {showDetails ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 19 19"
+                        strokeWidth="1.5"
+                        stroke="#003DA5"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19.5 12h-15"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 19 19"
+                        strokeWidth="1.5"
+                        stroke="#003DA5"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 6v12m6-6H6"
+                        />
+                      </svg>
+                    )}
                   </button>
                 </div>
 
