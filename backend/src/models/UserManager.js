426ccbb8d77bce7f9f2654862996c3aa1a58c220
Chloé Bidau <chloebidau@hotmail.fr>
2023-01-06 11:12:58 +0100

    little changes on authorization

--- backend/src/models/UserManager.js
+++ backend/src/models/UserManager.js
@@ -40,13 +40,13 @@ class UserManager extends AbstractManager {
 
   update(user) {
     return this.connection.query(
-      `update ${this.table} set firstname = ? , lastname = ? , email = ? , phone = ? , hashedPassword = ? , profilePicture = ? , level = ? , admin  = ?  where id = ?`,
+      `update ${this.table} set firstname = ? , lastname = ? , email = ? , phone = ? , password = ? , profilePicture = ? , level = ? , admin  = ?  where id = ?`,
       [
         user.firstname,
         user.lastname,
         user.email,
         user.phone,
-        user.hashedPassword,
+        user.password,
         user.profilePicture,
         user.level,
         user.admin,
