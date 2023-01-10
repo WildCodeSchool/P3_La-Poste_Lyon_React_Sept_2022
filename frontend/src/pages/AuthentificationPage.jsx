b141e52b33c472f8809d246fb67a250baac9e2f2
Marion Lalonde <lalondemarion.perso@gmail.com>
2023-01-10 15:42:09 +0100

    Correction after the PR, front end, some correction will be manage in the futur (backlogs)

--- frontend/src/pages/AuthentificationPage.jsx
+++ frontend/src/pages/AuthentificationPage.jsx
@@ -5,7 +5,6 @@ import granny from "../assets/granny1.svg";
 import arobase from "../assets/arobaselogo.png";
 import locker from "../assets/lockerlogo.png";
 import forgotpass from "../assets/forgotpass.svg";
-import Footer from "../components/Footer";
 
 function AuthentificationPage() {
   /* Get the context of the user (user informations + token) */
@@ -57,76 +56,73 @@ function AuthentificationPage() {
   };
 
   return (
-    <>
-      <form
-        onSubmit={handleSubmit}
-        className=" flex flex-col items-center justify-center  "
+    <form
+      onSubmit={handleSubmit}
+      className=" flex flex-col items-center justify-center  "
+    >
+      <h1 className=" items-center content-center justify-center text-3xl mb-10 mt-10">
+        CONNECTEZ-VOUS
+      </h1>
+
+      <div className=" flex md:w-3/5 justify-center ">
+        <label htmlFor="email" name="email">
+          <img src={arobase} alt="arobase" className="w-14 h-14 ml-1" />
+        </label>
+        <input
+          type="email"
+          pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+          placeholder="Entrez votre addresse email"
+          required
+          value={email}
+          onChange={handleChangeEmail}
+          id="email"
+          name="email"
+          className="bg-gray-200  text-gray-600   py-2 px-4 border rounded-2xl   md:w-3/5 "
+        />
+      </div>
+      <p className="italic text-gray-400 underline mb-4 ml-28 md:ml-80 text-xs md:text-sm md:mb-16">
+        adresse e-mail oubliée?
+      </p>
+
+      <div className="flex  justify-center md:w-3/5">
+        <label htmlFor="password" name="password">
+          <img src={locker} alt="locker" className="w-14 h-14 ml-1" />{" "}
+        </label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={handleChangePassword}
+          id="password"
+          name="password"
+          placeholder="Entrez votre mot de passe"
+          className="bg-gray-200  text-gray-600   py-2 px-4 border  rounded-2xl md:w-3/5 "
+        />
+      </div>
+      <p className="italic text-gray-400 underline mb-4 ml-28 md:ml-80 text-xs md:text-sm md:mb-16">
+        mot de passe oublié ?
+      </p>
+
+      <div className=" flex flex-col items-center justify-center ml-20 mr-20 md:mb-4">
+        <img
+          src={granny}
+          className=" max-w-sm w-64 h-64 md:hidden mb-8"
+          alt="granny"
+        />
+        <img
+          src={forgotpass}
+          className=" hidden w-21 h-31 mr-50 md:block mb-8 md:mb-9"
+          alt="forgotpass"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="bg-[#003DA5] text-white m-1 py-1 px-4 rounded-lg shadow-lg md:h-14 md:w-44 md:text-lg hover:shadow hover:bg-[#FFC927] hover:text-black mb-28"
       >
-        <h1 className=" items-center content-center justify-center text-3xl mb-10 mt-10">
-          CONNECTEZ-VOUS
-        </h1>
-
-        <div className=" flex ml-10 md:w-3/5 justify-center ">
-          <label htmlFor="email" name="email">
-            <img src={arobase} alt="arobase" className="w-14 h-14 ml1-1" />
-          </label>
-          <input
-            type="email"
-            pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
-            placeholder="Entrez votre addresse email"
-            required
-            value={email}
-            onChange={handleChangeEmail}
-            id="email"
-            name="email"
-            className="bg-gray-200 -4mb0 text-gray-600   py-2 px-4 border rounded-2xl   md:w-3/5 "
-          />
-        </div>
-        <p className="italic text-gray-400 underline mb-12 ml-40">
-          adresse e-mail oubliée?
-        </p>
-
-        <div className="flex justify-center md:w-3/5">
-          <label htmlFor="password" name="password">
-            <img src={locker} alt="locker" className="w-14 h-14 ml1-1" />{" "}
-          </label>
-          <input
-            type="password"
-            required
-            value={password}
-            onChange={handleChangePassword}
-            id="password"
-            name="password"
-            placeholder="Entrez votre mot de passe"
-            className="bg-gray-200 -4mb0 text-gray-600   py-2 px-4 border  rounded-2xl md:w-3/5 "
-          />
-        </div>
-        <p className="italic text-gray-400 underline mb-4 ml-28 md:ml-80 text-xs md:text-sm md:mb-16">
-          mot de passe oublié ?
-        </p>
-
-        <div className=" flex flex-col items-center justify-center ml-20 mr-20 md:mb-4">
-          <img
-            src={granny}
-            className=" max-w-sm w-64 h-64 md:hidden mb-8"
-            alt="granny"
-          />
-          <img
-            src={forgotpass}
-            className=" hidden w-21 h-31 mr-50 md:block mb-8 md:mb-9"
-            alt="forgotpass"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="bg-[#003DA5] text-white m-1 py-1 px-4 rounded-lg shadow-lg md:h-14 md:w-44 md:text-lg hover:shadow hover:bg-[#FFC927] hover:text-black mb-28"
-        >
-          Connexion
-        </button>
-      </form>
-      <Footer />
-    </>
+        Connexion
+      </button>
+    </form>
   );
 }
 
