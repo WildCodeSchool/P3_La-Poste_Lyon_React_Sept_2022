b956304fa43d16c6d819c3003f9cf436c20f0284
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2023-01-20 10:16:48 +0100

    ADD categoryControllers and Manager to get all status finished by category and total of tuto by category

--- backend/src/models/CategoryManager.js
+++ backend/src/models/CategoryManager.js
@@ -30,6 +30,26 @@ class CategoryManager extends AbstractManager {
       [category.name, category.icon, category.position, category.id]
     );
   }
+
+  getProgressionTuto(id) {
+    return this.connection.query(
+      `SELECT 
+    c.name AS 'category', 
+      COUNT(CASE WHEN ts.status = 'finished' AND u.id = ? THEN 1 END) AS 'tuto_completed',
+      (SELECT COUNT(*) FROM tuto WHERE category_id = c.id) as 'total_tuto'
+  FROM 
+      category c
+      LEFT JOIN tuto t ON c.id = t.category_id
+      LEFT JOIN tutorialStatus ts ON t.id = ts.tuto_id AND ts.status = 'finished'
+      LEFT JOIN user u ON ts.user_id = u.id
+  GROUP BY 
+      c.id, c.name
+  ORDER BY
+      c.id;
+  `,
+      [id]
+    );
+  }
 }
 
 module.exports = CategoryManager;
