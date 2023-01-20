9f54b92f183b0c8b38e5252b695b22744c986fd4
Marion Lalonde <lalondemarion.perso@gmail.com>
2023-01-20 12:37:17 +0100

    ProgressionBar OP

--- frontend/src/contexts/TutorialsContext.jsx
+++ frontend/src/contexts/TutorialsContext.jsx
@@ -19,7 +19,7 @@ export function TutorialsContextProvider({ children }) {
         });
     };
     getTutorials();
-  }, []);
+  }, [tutorials]);
 
   return (
     <TutorialsContext.Provider value={{ tutorials }}>
