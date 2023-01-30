971f947d8924d9baed8f8418cafa6732523ecdb1
arnaudchampetier <arnaud.champetier9@gmail.com>
2023-01-30 16:57:43 +0100

    Quiz fonctionnels

--- frontend/src/components/QuizTuto1.jsx
+++ frontend/src/components/QuizTuto1.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import celebration from "../assets/connexionPage/registerPage/winner.svg";
 import { useCurrentUserContext } from "../contexts/userContext";
 import jeux from "../assets/navBar/navBarUser/jeux.png";
+import PreviousButton from "./PreviousButton";
+import BannerProfile from "./BannerProfile";
 
 const { VITE_BACKEND_URL } = import.meta.env;
 
@@ -47,6 +49,7 @@ function QuizTuto1() {
     setCurrentQuestion(currentQuestion + 1);
     setAnswered(false);
   };
+  const navigate = useNavigate();
 
   const handleNext = (answer) => {
     setShowAnswer(false);
@@ -74,7 +77,10 @@ function QuizTuto1() {
   if (quizEnd) {
     return (
       <div>
-        <h2 className="bg-white text-3xl font-bold mx-auto w-full mt-4 rounded-lg flex py-4 justify-center">
+        <BannerProfile />
+        <PreviousButton />
+
+        <h2 className="bg-white text-3xl font-bold mx-auto w-full mt-12 rounded-lg flex py-4 justify-center">
           Vous avez terminé le quiz !
         </h2>
         <p className="text-center mt-12 font-bold text-xl">
@@ -83,16 +89,24 @@ function QuizTuto1() {
         <img
           src={celebration}
           alt="youpi"
-          className="w-full h-full md:w-1/3 md:h-1/3 mx-auto"
+          className="w-full h-full md:w-1/4 md:h-1/4 mx-auto"
         />
-
-        <button
-          className=" flex items-center bg-gray-900 mx-auto  rounded-lg p-4 font-bold text-white hover:bg-main-blue  my-8"
-          type="button"
-          onClick={handleReset}
-        >
-          Recommencer
-        </button>
+        <div className="flex justify-center">
+          <button
+            className=" flex items-center bg-main-blue mx-4  rounded-lg p-4 font-bold text-white hover:bg-main-yellow  my-8"
+            type="button"
+            onClick={handleReset}
+          >
+            Recommencer
+          </button>
+          <button
+            className=" flex items-center bg-main-blue mx-4 rounded-lg p-4 font-bold text-white hover:bg-main-yellow  my-8"
+            type="button"
+            onClick={() => navigate(-1)}
+          >
+            Retour à mes quiz
+          </button>
+        </div>
       </div>
     );
   }
@@ -100,83 +114,93 @@ function QuizTuto1() {
   // const currentQ = questions[currentQuestion];
 
   return (
-    <div className="w-3/4 h-1/2 items-center justify-center flex flex-col mx-auto bg-white rounded-lg shadow-lg p-6 my-4">
-      {quizData?.title && (
-        <div>
-          <h1 className="text-3xl animate-pulse">{quizData.title}</h1>
-          <img src={jeux} alt={quizData.title} className="w-full h-full " />
-        </div>
-      )}
-      {quizData.questions && quizData.questions.length > 0 ? (
-        <>
-          <p className="bg-white font-bold mx-auto w-1/2 mt-4 rounded-lg flex py-4 justify-center">
-            {currentQuestion + 1}.{" "}
-            {quizData.questions[currentQuestion]?.question}
-          </p>
-          {quizData?.questions[currentQuestion]?.responses?.map(
-            (response, responseIndex) => (
+    <div>
+      <BannerProfile />
+      <PreviousButton />
+      <div className="w-full md:w-1/2 h-full items-center justify-center flex flex-col mx-auto bg-white rounded-lg md:shadow-lg p-6 my-4 ">
+        {quizData?.title && (
+          <div>
+            <h1 className="text-3xl">{quizData.title}</h1>
+            <img src={jeux} alt={quizData.title} className="w-full h-full " />
+          </div>
+        )}
+        {quizData.questions && quizData.questions.length > 0 ? (
+          <>
+            <p className="bg-white font-bold mx-auto w-1/2 mt-4 rounded-lg flex py-4 justify-center">
+              {currentQuestion + 1}.{" "}
+              {quizData.questions[currentQuestion]?.question}
+            </p>
+            {quizData?.questions[currentQuestion]?.responses?.map(
+              (response, responseIndex) => (
+                <button
+                  type="button"
+                  key={response.id}
+                  className={`ml-4 text-base cursor-pointer flex md:ml-96 rounded-lg p-4 font-bold text-white  md:mr-96 bg-main-yellow hover:bg-main-blue my-4 md:w-1/3 w-full ${
+                    answered && response.isCorrect ? "font-bold" : ""
+                  }`}
+                  onClick={() => {
+                    setAnswered(true);
+                    setShowAnswer(true);
+                    setDisabled(true);
+                    // eslint-disable-next-line no-unused-expressions
+                    response.isCorrect
+                      ? (handleNext(response), setScore(score + 1))
+                      : handleNext(response);
+                  }}
+                >
+                  {responseIndex + 1}. {response.content}
+                  {showAnswer && (
+                    <span
+                      className={`ml-4 ${
+                        response.isCorrect ? "text-green-500" : "text-red-500"
+                      }`}
+                    >
+                      {response.isCorrect ? "Correct" : "Incorrect"}
+                    </span>
+                  )}
+                </button>
+              )
+            )}
+            {showAnswer && !quizEnd && (
               <button
+                className="flex items-center bg-main-blue mx-auto rounded-lg p-4 font-bold text-white mt-8 hover:bg-gray-900"
                 type="button"
-                key={response.id}
-                className={`ml-4 text-base cursor-pointer flex md:ml-56 rounded-lg p-4 font-bold text-white  md:mr-96 bg-main-yellow hover:bg-main-blue my-4 md:w-1/3 w-full ${
-                  answered && response.isCorrect ? "font-bold" : ""
-                }`}
-                onClick={() => {
-                  setAnswered(true);
-                  setShowAnswer(true);
-                  setDisabled(true);
-                  // eslint-disable-next-line no-unused-expressions
-                  response.isCorrect
-                    ? handleNext(response)
-                    : handleNext(response);
-                }}
+                onClick={handleNextQuestion}
+                disabled={disabled}
               >
-                {responseIndex + 1}. {response.content}
-                {showAnswer && (
-                  <span
-                    className={`ml-4 ${
-                      response.isCorrect ? "text-green-500" : "text-red-500"
-                    }`}
-                  >
-                    {response.isCorrect ? "Correct" : "Incorrect"}
-                  </span>
-                )}
+                Question suivante
               </button>
-            )
-          )}
-          {showAnswer && !quizEnd && (
-            <button
-              className="flex items-center bg-main-blue mx-auto rounded-lg p-4 font-bold text-white mt-8 hover:bg-gray-900"
-              type="button"
-              onClick={handleNextQuestion}
-              disabled={disabled}
-            >
-              Question suivante
-            </button>
-          )}
-        </>
-      ) : (
-        <p>Aucune question disponible</p>
-      )}
-      {quizEnd && (
-        <div>
-          <h2 className="bg-white text-3xl font-bold mx-auto w-full mt-4 rounded-lg flex py-4 justify-center">
-            Vous avez terminé le quiz !
-          </h2>
-          <p className="text-center mt-12 text-semibold text-xl">
-            Votre score : {score}/{quizData.length}
-          </p>
-          <img src={celebration} alt="youpi" className="w-1/5 h-1/5 mx-auto" />
+            )}
+          </>
+        ) : (
+          <p>Aucune question disponible</p>
+        )}
+        {quizEnd && (
+          <div>
+            <BannerProfile />
 
-          <button
-            className=" flex items-center bg-gray-900 mx-auto  rounded-lg p-4 font-bold text-white hover:bg-main-blue  my-8"
-            type="button"
-            onClick={handleReset}
-          >
-            Recommencer
-          </button>
-        </div>
-      )}
+            <h2 className="bg-white text-3xl font-bold mx-auto w-full mt-4 rounded-lg flex flex-col py-4 justify-center">
+              Vous avez terminé le quiz !
+            </h2>
+            <p className="text-center mt-12 text-semibold text-xl">
+              Votre score : {score}/{quizData.length}
+            </p>
+            <img
+              src={celebration}
+              alt="youpi"
+              className="w-1/5 h-1/5 mx-auto"
+            />
+
+            <button
+              className=" flex items-center bg-gray-900 mx-auto  rounded-lg p-4 font-bold text-white hover:bg-main-blue  my-8"
+              type="button"
+              onClick={handleReset}
+            >
+              Recommencer
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -48,14 +48,14 @@ function Games() {
           Mes Quiz
         </h1>
       </div>
-      <ul className="flex flex-col  overflow-hidden py-[1vh] mx-[12vw]">
+      <ul className="grid grid-cols-1 md:grid-cols-2  overflow-hidden py-[1vh] mx-[12vw]">
         {/* <li> elements are contained in <Link> to redirect the user to the corresponding page */}
 
         {quizList.length > 0 &&
           quizList.map((quiz) => (
             <li
               key={quiz.id}
-              className="bg-white box row-start-1 row-span-1 col-start-1 col-span-1 my-3 md:m-6 border shadow-xl rounded-xl text-center h-65 md:box md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1"
+              className="bg-white box  md:m-6 border shadow-xl rounded-xl text-center h-65 md:box "
             >
               <h2 className="text-xl md:text-2xl text-main-black  font-bold py-4 bg-white  rounded-tl-lg rounded-tr-lg h-17 flex justify-center items-center">
                 {quiz.title}
@@ -75,39 +75,3 @@ function Games() {
 }
 
 export default Games;
-
-/* <li className="bg-white box row-start-2 row-span-1 col-start-1 col-span-1 my-3 md:m-6 border shadow-xl rounded-lg text-center h-65 md:box md:row-start-2 md:row-span-1 md:col-start-1 md:col-span-1">
-<h2 className="text-xl md:text-2xl text-main-black  font-bold py-4 bg-white  rounded-tl-lg rounded-tr-lg h-17 flex justify-center items-center">
-  Vie courante{" "}
-</h2>
-<hr />
-<div className="flex justify-center my-5">
-  <img
-    className="h-20"
-    src="src/assets/tutorial-category-img/currentlife.svg"
-    alt="mestutos"
-  />
-</div>
-<Link to="/quiz2">
-  <AccessButton />
-</Link>
-</li>
-
-<li className="bg-white box row-start-3 row-span-1 col-start-1 col-span-2 my-3 md:m-6 border shadow-xl rounded-lg text-center h-65 md:box md:row-start-1 md:row-span-1 md:col-start-2 md:col-span-2">
-<h2 className="text-xl md:text-2xl text-main-black  font-bold py-4 bg-white  rounded-tl-lg rounded-tr-lg h-17 flex justify-center items-center">
-  Utiliser un ordinateur
-</h2>
-<hr />
-<div className="flex flex-col justify-center h-max">
-  <div className="flex justify-center my-5">
-    <img
-      className="h-20"
-      src="src/assets/tutorial-category-img/desktop.svg"
-      alt="mestutos"
-    />
-  </div>
-  <Link to="/quiz3" className="">
-    <AccessButton />
-  </Link>
-</div>
-</li> */
