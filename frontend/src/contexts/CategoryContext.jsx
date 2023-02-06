14958303edb78d381c8087d0e676cbd5416a99e1
Chloé Bidau <chloebidau@hotmail.fr>
2023-02-06 15:48:12 +0100

    pattern email k

--- frontend/src/contexts/CategoryContext.jsx
+++ frontend/src/contexts/CategoryContext.jsx
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useEffect } from "react";
-import { toast, Toaster } from "react-hot-toast";
 
 const { VITE_BACKEND_URL } = import.meta.env;
 
@@ -7,11 +6,6 @@ const CategoryContext = createContext();
 export { CategoryContext };
 
 export function CategoryContextProvider({ children }) {
-  const notifyProblem = () =>
-    toast(
-      "There was a problem fetching the categories. Please try again later."
-    );
-
   /* fetch categories from localhost */
   const [categories, setCategories] = useState([]);
 
@@ -21,9 +15,6 @@ export function CategoryContextProvider({ children }) {
         .then((response) => response.json())
         .then((data) => {
           setCategories(data);
-        })
-        .catch((err) => {
-          notifyProblem(err);
         });
     };
     getCategories();
@@ -31,8 +22,6 @@ export function CategoryContextProvider({ children }) {
 
   return (
     <CategoryContext.Provider value={{ categories }}>
-      <Toaster position="top-center" reverseOrder />
-
       {children}
     </CategoryContext.Provider>
   );
